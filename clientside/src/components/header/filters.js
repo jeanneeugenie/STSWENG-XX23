d28f85e77945216d3a23cd80db7fe9f95b0f53c5
BhoScXZ WOzH <nauj_krylle_e_agbayani@dlsu.edu.ph>
2024-12-03 19:57:46 +0800

    pushing a revised homepage for ride postings

--- clientside/src/components/header/filters.js
+++ clientside/src/components/header/filters.js
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
 import { Box, Grid, TextField, FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material';
 
-
 const FilterSection = ({ onApplyFilter }) => {
     // declare as blank selection first
   const [driver, setDriver] = useState('');
   const [rating, setRating] = useState('');
+  const [pickupPoint, setPickupPoint] = useState('');
   const [destination, setDestination] = useState('');
   const [startHour, setStartHour] = useState('');
   const [endHour, setEndHour] = useState('');
 
   const handleApplyFilter = () => {
-    const filters = { driver, rating, destination, startHour, endHour };
+    const filters = { driver, rating, pickupPoint, destination, startHour, endHour };
     onApplyFilter(filters);
   };
 
   return (
-    <Box sx={{ padding: 2, backgroundColor: 'white', color: 'white' }}>
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12} sm={6} md={3}>
+    <Box sx={{ padding: 2, backgroundColor: 'white' }}>
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        {/* Driver Filter */}
+        <Grid item xs={12} sm={6} md={2}>
           <FormControl fullWidth>
             <InputLabel sx={{ color: '#3A5940' }}>Driver</InputLabel>
             <Select
@@ -32,7 +33,9 @@ const FilterSection = ({ onApplyFilter }) => {
             </Select>
           </FormControl>
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
+
+        {/* Rating Filter */}
+        <Grid item xs={12} sm={6} md={2}>
           <FormControl fullWidth>
             <InputLabel sx={{ color: '#3A5940' }}>Min. Rating</InputLabel>
             <Select
@@ -43,28 +46,56 @@ const FilterSection = ({ onApplyFilter }) => {
               <MenuItem value="">All</MenuItem>
               <MenuItem value="5">5 Stars</MenuItem>
               <MenuItem value="4">4 Stars</MenuItem>
+              <MenuItem value="3">3 Stars</MenuItem>
+              <MenuItem value="2">2 Stars</MenuItem>
+              <MenuItem value="1">1 Star</MenuItem>
             </Select>
           </FormControl>
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
+
+        {/* Pick-up Point Filter */}
+        <Grid item xs={12} sm={6} md={2}>
           <FormControl fullWidth>
-            <InputLabel sx={{ color: '#3A5940' }}>Drop off</InputLabel>
+            <InputLabel sx={{ color: '#3A5940' }}>Pick-up Point</InputLabel>
+            <Select
+              value={pickupPoint}
+              onChange={(e) => setPickupPoint(e.target.value)}
+              sx={{ backgroundColor: 'white', color: '#3A5940' }}
+            >
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="George Ty Building">George Ty Building</MenuItem>
+              <MenuItem value="Richard Lee Block">Richard Lee Block</MenuItem>
+              <MenuItem value="Milagros R. del Rosario Building">Milagros R. del Rosario Building</MenuItem>
+              <MenuItem value="Enrique Razon Building">Enrique Razon Building</MenuItem>
+              <MenuItem value="University Hall">University Hall</MenuItem>
+              <MenuItem value="Covered Court/West Canopy">Covered Court/West Canopy</MenuItem>
+              <MenuItem value="LC2 Annex">LC2 Annex</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+
+        {/* Destination Filter */}
+        <Grid item xs={12} sm={6} md={2}>
+          <FormControl fullWidth>
+            <InputLabel sx={{ color: '#3A5940' }}>Destination</InputLabel>
             <Select
               value={destination}
               onChange={(e) => setDestination(e.target.value)}
               sx={{ backgroundColor: 'white', color: '#3A5940' }}
             >
               <MenuItem value="">All</MenuItem>
-              <MenuItem value="MRR">MRR</MenuItem>
-              <MenuItem value="GT">GT Building</MenuItem>
-              <MenuItem value="RL">RL Building</MenuItem>
+              <MenuItem value="Paseo Outlets">Paseo Outlets</MenuItem>
+              <MenuItem value="Vista Mall Sta. Rosa">Vista Mall Sta. Rosa</MenuItem>
+              {/* Add more destination options here */}
             </Select>
           </FormControl>
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
+
+        {/* Start Hour Filter */}
+        <Grid item xs={12} sm={6} md={2}>
           <TextField
             type="number"
-            label="Ride Pickup Hour (Earliest)"
+            label="Pick-up Start Hour"
             value={startHour}
             onChange={(e) => setStartHour(e.target.value)}
             sx={{ backgroundColor: 'white', color: '#3A5940' }}
@@ -72,10 +103,12 @@ const FilterSection = ({ onApplyFilter }) => {
             InputProps={{ inputProps: { min: 0, max: 23 } }}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
+
+        {/* End Hour Filter */}
+        <Grid item xs={12} sm={6} md={2}>
           <TextField
             type="number"
-            label="Ride Pickup Hour (Latest)"
+            label="Pick-up End Hour"
             value={endHour}
             onChange={(e) => setEndHour(e.target.value)}
             sx={{ backgroundColor: 'white', color: '#3A5940' }}
@@ -84,11 +117,17 @@ const FilterSection = ({ onApplyFilter }) => {
           />
         </Grid>
       </Grid>
+
+      {/* Apply Filter Button */}
       <Box sx={{ textAlign: 'center', marginTop: 2 }}>
         <Button
           variant="contained"
           onClick={handleApplyFilter}
-          sx={{ backgroundColor: '#3A5940', '&:hover': { backgroundColor: '#578158' } }}
+          sx={{
+            backgroundColor: '#3A5940',
+            '&:hover': { backgroundColor: '#578158' },
+            textTransform: 'none',
+          }}
         >
           Apply Filter
         </Button>
--- clientside/src/pages/HomePage.js
+++ clientside/src/pages/HomePage.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import RidePostCard from '../components/cards/ridepost';
 import FilterSection from '../components/header/filters';
-import {Button, Dialog, DialogContent, DialogActions, TextField, Typography, Rating} from '@mui/material';
+import { Button, Dialog, DialogContent, DialogActions, TextField, Typography, Rating, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
+
 const HomePage = ({ isDriver }) => {
     const centeredContainer = {
         display: 'flex',
@@ -10,6 +11,7 @@ const HomePage = ({ isDriver }) => {
         justifyContent: 'center',
         minHeight: '100vh'
     };
+
     const stickyButton = {
         position: 'sticky',
         bottom: '2%',
@@ -17,6 +19,7 @@ const HomePage = ({ isDriver }) => {
         display: 'flex',
         justifyContent: 'flex-end',
     };
+
     const [filters, setFilters] = useState({});
     const [dialogState, setDialogState] = useState(false);
     const [selectedRide, setSelectedRide] = useState(null);
@@ -24,8 +27,8 @@ const HomePage = ({ isDriver }) => {
     const [bookedRide, setBookedRide] = useState(null);
 
     const tempData = [
-        { id: 1, driver: 'Driver A', rating: 5, pickupPoint: 'GT Building', destination: 'Paseo Outlets', pickupHour: '12', pickupMinute: '5', carDetails: 'ABC 1234, White Toyota Fortuner', maxPass: 4},
-        { id: 2, driver: 'Driver B', rating: 4, pickupPoint: 'MRR', destination: 'Vista Mall Sta. Rosa', pickupHour: '10', pickupMinute: '30', carDetails: 'DEF 5678, Black Isuzu D-Max', maxPass: 3},
+        { id: 1, driver: 'Driver A', rating: 5, pickupPoint: 'George Ty Building', destination: 'Paseo Outlets', pickupHour: '12', pickupMinute: '5', carDetails: 'ABC 1234, White Toyota Fortuner', maxPass: 4 },
+        { id: 2, driver: 'Driver B', rating: 4, pickupPoint: 'Richard Lee Block', destination: 'Vista Mall Sta. Rosa', pickupHour: '10', pickupMinute: '30', carDetails: 'DEF 5678, Black Isuzu D-Max', maxPass: 3 },
     ];
 
     const filterRides = tempData.filter((ride) => {
@@ -38,6 +41,7 @@ const HomePage = ({ isDriver }) => {
             (!filters.endHour || ride.pickupHour <= parseInt(filters.endHour, 10))
         );
     });
+
     const openDialog = () => {
         setDialogState(true);
     };
@@ -45,6 +49,7 @@ const HomePage = ({ isDriver }) => {
     const closeDialog = () => {
         setDialogState(false);
     };
+
     const openRideDetails = (ride) => {
         setSelectedRide(ride);
         setDetailsDialogOpen(true);
@@ -54,33 +59,54 @@ const HomePage = ({ isDriver }) => {
         setDetailsDialogOpen(false);
         setSelectedRide(null);
     };
+
     const getRide = (rideDetails) => {
         setBookedRide(rideDetails);
-        console.log(bookedRide)
-        //alert(`You have booked the ride to ${rideDetails.destination}`);
+        console.log(bookedRide);
     };
+
+    // Function to validate minutes between 00 and 59
+    const handleMinuteChange = (e) => {
+        const value = e.target.value;
+        if (value >= 0 && value <= 59) {
+            setFilters({ ...filters, pickupMinute: value });
+        }
+    };
+
     return (
-        
         <div>
             <FilterSection onApplyFilter={setFilters} />
             <div style={centeredContainer}>
                 {filterRides.map((ride) => (
-                <RidePostCard key={ride.id} rating = {ride.rating} destination = {ride.destination} pickupPoint = {ride.pickupPoint} 
-                driver = {ride.driver} pickupHour = {ride.pickupHour} pickupMinute = {ride.pickupMinute} getRide={getRide} carDetails = {ride.carDetails} maxPass = {ride.maxPass}
-                openRideDetails={() => openRideDetails(ride)}/>
+                    <RidePostCard 
+                        key={ride.id}
+                        rating={ride.rating} 
+                        destination={ride.destination} 
+                        pickupPoint={ride.pickupPoint} 
+                        driver={ride.driver} 
+                        pickupHour={ride.pickupHour} 
+                        pickupMinute={ride.pickupMinute} 
+                        getRide={getRide} 
+                        carDetails={ride.carDetails} 
+                        maxPass={ride.maxPass}
+                        openRideDetails={() => openRideDetails(ride)}
+                    />
                 ))}
             </div>
-            { isDriver && (
-            <div style={stickyButton}>
-                <Button variant="contained"  onClick={openDialog}
-                sx={{
-                    backgroundColor: '#3A5940',
-                    '&:hover': {
-                        backgroundColor: '#578158',
-                    },
-                }}>
-                Post Ride </Button>
-            </div> 
+            {isDriver && (
+                <div style={stickyButton}>
+                    <Button 
+                        variant="contained"  
+                        onClick={openDialog}
+                        sx={{
+                            backgroundColor: '#3A5940',
+                            '&:hover': {
+                                backgroundColor: '#578158',
+                            },
+                        }}>
+                        Post Ride 
+                    </Button>
+                </div> 
             )}
             <Dialog open={dialogState} onClose={closeDialog}>
                 <DialogContent
@@ -89,8 +115,7 @@ const HomePage = ({ isDriver }) => {
                         flexDirection: 'column',
                         alignItems: 'center',
                         padding: '2rem',
-                    }}
-                >
+                    }}>
                     <Typography variant="h6">
                         Ride destination
                     </Typography>
@@ -103,33 +128,53 @@ const HomePage = ({ isDriver }) => {
                     <Typography variant="h6">
                         Ride pick-up point
                     </Typography>
-                    <TextField
-                        fullWidth
-                        label="Pick-up Point"
-                        variant="outlined"
-                        margin="dense"
-                    />
+                    <FormControl fullWidth margin="dense">
+                        <InputLabel>Pick-up Point</InputLabel>
+                        <Select
+                            value={filters.pickupPoint || ''}
+                            onChange={(e) => setFilters({ ...filters, pickupPoint: e.target.value })}
+                            label="Pick-up Point"
+                        >
+                            <MenuItem value="George Ty Building">George Ty Building</MenuItem>
+                            <MenuItem value="Richard Lee Block">Richard Lee Block</MenuItem>
+                            <MenuItem value="Milagros R. del Rosario Building">Milagros R. del Rosario Building</MenuItem>
+                            <MenuItem value="Enrique Razon Building">Enrique Razon Building</MenuItem>
+                            <MenuItem value="University Hall">University Hall</MenuItem>
+                            <MenuItem value="Covered Court/West Canopy">Covered Court/West Canopy</MenuItem>
+                            <MenuItem value="LC2 Annex">LC2 Annex</MenuItem>
+                        </Select>
+                    </FormControl>
+
                     <Typography variant="h6">
                         Pick-up Schedule (in Military Time)
                     </Typography>
-                    <TextField
-                        type = "number"
-                        fullWidth
-                        label="Pick-up Time (Hour)"
-                        variant="outlined"
-                        margin="dense"
-                        sx={{ marginBottom: '1rem' }}
-                    />
+                    <FormControl fullWidth margin="dense">
+                        <InputLabel>Pick-up Hour</InputLabel>
+                        <Select
+                            value={filters.pickupHour || ''}
+                            onChange={(e) => setFilters({ ...filters, pickupHour: e.target.value })}
+                            label="Pick-up Hour"
+                        >
+                            {[...Array(24).keys()].map((hour) => (
+                                <MenuItem key={hour} value={hour}>
+                                    {hour < 10 ? `0${hour}` : hour}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+
                     <Typography variant="h6">
                         Pick-up Schedule (minutes)
                     </Typography>
                     <TextField
-                        type = "number"
+                        type="number"
                         fullWidth
                         label="Pick-up Time (minute)"
                         variant="outlined"
                         margin="dense"
-                        sx={{ marginBottom: '1rem' }}
+                        value={filters.pickupMinute || ''}
+                        onChange={handleMinuteChange}
+                        inputProps={{ min: 0, max: 59 }}
                     />
                     <Typography variant="h6">
                         Vehicle Information (Identification)
@@ -163,15 +208,13 @@ const HomePage = ({ isDriver }) => {
                                     backgroundColor: '#578158',
                                 },
                                 textTransform: 'none',
-                            }}
-                        >
+                            }}>
                             Post Ride
                         </Button>
                     </DialogActions>
                 </DialogContent>
             </Dialog>
 
-                                
             <Dialog open={detailsDialogOpen} onClose={closeDetails}>
                 <DialogContent
                     sx={{
@@ -179,14 +222,13 @@ const HomePage = ({ isDriver }) => {
                         flexDirection: 'column',
                         alignItems: 'left',
                         padding: '2rem',
-                    }}
-                >
+                    }}>
                     <Rating
-                    name="driver-rating"
-                    value={selectedRide?.rating || 0}
-                    readOnly
-                    precision={0.5}
-                    size="medium"
+                        name="driver-rating"
+                        value={selectedRide?.rating || 0}
+                        readOnly
+                        precision={0.5}
+                        size="medium"
                     />
                     <Typography variant="h5" sx={{ fontWeight: 'bold'}}>
                         {selectedRide?.destination}
@@ -217,16 +259,13 @@ const HomePage = ({ isDriver }) => {
                                     backgroundColor: '#578158',
                                 },
                                 textTransform: 'none',
-                            }}
-                        >
+                            }}>
                             Book Ride
-                            
                         </Button>
                     </DialogActions>
                 </DialogContent>
             </Dialog>
         </div>
-        
     );
 }
 
